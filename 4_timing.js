c3875c5b22165741bd0dd8efc3fcf90ee584f4f1
Divyansh Jha <divyanshj.16@gmail.com>
2017-08-09 16:23:27 +0530

    add percentage wait time on console

--- 4_timing.js
+++ 4_timing.js
@@ -1,16 +1,28 @@
 var waitTime = 3000;
 var currentTime = 0;
-var waitInterval = 500;
+var waitInterval = 10;
+var percentWaited = 0;
 
-console.log("Wait for it!");
+// console.log("Wait for it!");
+function writeWaitingPercentage(p) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`waiting ... ${p}%`);
+}
 
 var interval = setInterval(function() {
     currentTime += waitInterval;
-    console.log(`Waiting ${currentTime/1000} Seconds...`);
+    percentWaited = Math.floor((currentTime/waitTime)*100)
+    writeWaitingPercentage(percentWaited);
+    // console.log(`Waiting ${currentTime/1000} Seconds...`);
 },waitInterval);
 
 setTimeout(function() {
     clearInterval(interval)
-    console.log('done');
+    writeWaitingPercentage(100)
+    console.log('\n\ndone\n');
     // process.exit()
 },waitTime);
+
+process.stdout.write('\n')
+writeWaitingPercentage(0)
